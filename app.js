6c3770ece9b9209b4b196b58b3179314c3c4fbf6
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-02-16 18:34:02 -0600

    compelted the showing generic errors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const app = express();
 const itemRoutes = require("./itemRoutes");
+const { NotFoundError } = require("./expressError");
 
 // Get a middleware function and register it for all routes
 app.use(express.json());
@@ -11,5 +12,17 @@ app.use(express.json());
 app.use("/items", itemRoutes);
 
 
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res) {
+  throw new NotFoundError();
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
 
 module.exports = app;
--- itemRoutes.js
+++ itemRoutes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-
+const { BadRequestError } = require("./expressError");
 const db = require("./fakeDb");
 const router = new express.Router();
 
@@ -16,13 +16,15 @@ router.get("/", function (req, resp) {
 });
 
 /** return single item */
-router.get("/:name", function(req, resp) {
+router.get("/:name", function (req, resp) {
   let itemNameValue = req.params.name;
-  for(let item of db.items){
-    if(item.name === itemNameValue) {
+  for (let item of db.items) {
+    if (item.name === itemNameValue) {
       return resp.send(item);
     }
   }
+  throw new BadRequestError(
+    `It's a bad request, ${itemNameValue} is not in the list!`);
 });
 
 
@@ -30,17 +32,45 @@ router.get("/:name", function(req, resp) {
  * {name: "popsicle", price: 1.45} =>
   {added: {name: "popsicle", price: 1.45}}
 */
-router.post("/", function(req, resp){
+router.post("/", function (req, resp) {
   db.items.push(req.body);
   return resp.send({
     added: req.body,
   });
-} );
+});
 
 /** accept JSON body, modify item, return it */
-router.patch("/:name");
+router.patch("/:name", function (req, resp) {
+  const itemNameValue = req.params.name;
+  const updateValues = req.body;
+
+  for (let item of db.items) {
+    if (item.name === itemNameValue) {
+      item.name = updateValues.name;
+      item.price = updateValues.price;
+      return resp.send(
+        {
+          updated: item
+        });
+    }
+  }
+  throw new BadRequestError(
+    `It's a bad request, ${itemNameValue} is not in the list!`);
+});
 
 /** delete item */
-router.delete("/:name");
+router.delete("/:name", function (req, resp) {
+  const itemNameValue = req.params.name;
+
+  for (let item of db.items) {
+    if (item.name === itemNameValue) {
+      const itemIndex = db.items.indexOf(item);
+      db.items.splice(itemIndex,1);
+      return resp.send({message: "Deleted"});
+    }
+  }
+  throw new BadRequestError(
+    `It's a bad request, ${itemNameValue} is not in the list!`);
+});
 
 module.exports = router;
